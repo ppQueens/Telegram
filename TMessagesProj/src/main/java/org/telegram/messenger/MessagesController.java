d9b0f5810af7c8d9f9cc54661fc3cef746bf56ad
ppQueens <peterfromqueens@gmail.com>
2025-08-25 18:19:12 +0300

    Iterate over activated accounts in MessagesController push and logout flows

--- TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
@@ -6,3 +6,11 @@
             for (int a = 0; a < UserConfig.MAX_ACCOUNT_COUNT; a++) {
                 for (int a = 0; a < UserConfig.MAX_ACCOUNT_COUNT; a++) {
         for (int a = 0; a < UserConfig.MAX_ACCOUNT_COUNT; a++) {
+            for (int a : UserConfig.getActivatedAccounts()) {
+                if (a != currentAccount) {
+                    req.other_uids.add(UserConfig.getInstance(a).getClientUserId());
+                for (int a : UserConfig.getActivatedAccounts()) {
+                    if (a != currentAccount) {
+        for (int a : UserConfig.getActivatedAccounts()) {
+            if (a != currentAccount) {
+                long uid = UserConfig.getInstance(a).getClientUserId();
