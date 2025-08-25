6047825d5753a768cfdd4f44ec8118ad39603dfa
ppQueens <peterfromqueens@gmail.com>
2025-08-25 18:13:59 +0300

    Revert unnecessary MessagesController overhaul

--- TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
@@ -2,3 +2,7 @@
             for (int a : UserConfig.getActivatedAccounts()) {
                 for (int a : UserConfig.getActivatedAccounts()) {
         for (int a : UserConfig.getActivatedAccounts()) {
+        for (int i = 0; i < UserConfig.MAX_ACCOUNT_COUNT; i++) {
+            for (int a = 0; a < UserConfig.MAX_ACCOUNT_COUNT; a++) {
+                for (int a = 0; a < UserConfig.MAX_ACCOUNT_COUNT; a++) {
+        for (int a = 0; a < UserConfig.MAX_ACCOUNT_COUNT; a++) {
